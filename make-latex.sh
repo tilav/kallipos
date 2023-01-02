8eedcc359c0baf055333f50b1b6bfdc733c65e6b
epidrome <github@epidro.me>
2023-01-02 23:40:21 +0200

    updated

--- make-latex.sh
+++ make-latex.sh
@@ -9,7 +9,8 @@ for filename in text/ch*.txt; do
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --lua-filter=footnote.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --wrap=none --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
-pandoc text/epi.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/epi.tex
+pandoc text/web.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/web.tex
+pandoc text/bio.txt --top-level-division=chapter --to latex > latex/bio.tex
 
 for filename in text/apx*.txt; do
    [ -e "$filename" ] || continue
